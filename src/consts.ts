671bbf58fee136a2b433f0c16fb972fa95e96a98
kiritokatklian <casillaskhoren1@gmail.com>
2026-06-02 00:55:21 +0200

    chore: set up the blog

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const NAME = 'Kirito';
+export const SITE_TITLE = 'Kirito';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GITHUB_URL = 'https://github.com/kiritokatklian';
+export const TWITTER_URL = 'https://twitter.com/katklian';
